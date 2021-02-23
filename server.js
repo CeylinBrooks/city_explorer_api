a884d769e90c67506126ff8c98388fb17a1021e9
ceylinbrooks <ceylinbrooks@gmail.com>
2021-02-23 15:41:16 -0500

    locationHandler work?

--- server.js
+++ server.js
@@ -18,22 +18,45 @@ function dataCallback(request, response){
 
 app.get('/weather', handleWeather);
 app.get('/location', handleLocation)
+app.get('/yelp', handleYelp);
 
 function handleWeather(req, res){
-  console.log
   const jsonData = require('.data/weather.json');
+  for ()
   const result = new Weather (jsonData, req.query);
   res.send(result);
 }
 
 function Weather(jsonData, weatherStatus){
-  this.
+  this.forecast = jsonData.weather.desription;
   this.
   this.
 }; 
 
+//
 
+// ```
+// [
+//   {
+//     "forecast": "Partly cloudy until afternoon.",
+//     "time": "Mon Jan 01 2001"
+//   },
+//   {
+//     "forecast": "Mostly cloudy in the morning.",
+//     "time": "Tue Jan 02 2001"
+//   },
+//   ...
+// ]
+// ```
+//
+function handleLocation(req,res){
+    this.search_query = 'seattle';
+    this.formatted_query = "Seattle, WA, USA";
+    this.latitude = '47.606210';
+    this.longitude = '-122.332071';
+ 
+}
 
-
+function handleYelp(req,res)
 
 app.listen(3001,()=> console.log(`Now listening on PORT 3001`));
