9b29686aae1add6344148028584e44e649b577d5
ceylinbrooks <ceylinbrooks@gmail.com>
2021-02-22 22:34:06 -0500

    Location Handler Done

--- server.js
+++ server.js
@@ -33,6 +33,24 @@ function Location (locationData, cityDescrip){
   this.longitude = locationData[0].lon;
 }
 
+app.get('/restaurants', restaurantHandler);
+
+function restaurantHandler(req,res){
+  const restaurantJSON = require('.data/restaturants.json');
+
+  const output = [];
+  for (let i = 0; i < restaurantJSON.nearby_restaurants.length; i++){
+    output.push(new Restaurant(restaurantJSON.nearby_restaurants[i].restaurant))
+  }
+  res.send(output);
+}
+
+function Restaurant(object){
+  this.name = object.name;
+  this.area = object.location.locality_verbose;
+  this.cuisines = object.cuisines;
+}
+
 app.get('weather', handleWeather);
 function handleWeather(req, res){
   //console.log
