e1c323b733cf2765d8dc0dfcfc444ec26f152e40
ceylinbrooks <ceylinbrooks@gmail.com>
2021-02-24 23:58:21 -0500

    added SQL

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE city_explorer_table;
+
+CREATE TABLE city_explorer_table (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formattted_query VARCHAR(255),
+  latitude NUMERIC(10,7),
+  longitude NUMERIC(10,7)
+
+)
+
+-- NO trailing commas!
